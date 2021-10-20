ee0241cc814f9258d53687c6870d678dbec722c1
Kirill <freshjs.ftd@gmail.com>
2021-10-20 16:45:02 +0300

    Fix unintended bug

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -4,8 +4,6 @@ import { createUserSaga, getUserSaga, deleteUserSaga } from './usersSagas';
 
 function* rootSaga() {
   yield takeLatest(ACTION_TYPES.CREATE_USER_REQUEST, createUserSaga);
-  yield takeLatest(ACTION_TYPES.GET_USER_REQUEST, getUserSaga);
-  yield takeLatest(ACTION_TYPES.DELETE_USER_REQUEST, deleteUserSaga);
 }
 
 export default rootSaga;
