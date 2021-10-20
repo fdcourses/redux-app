b583033793505850e7a818d5e61a72ef41f2333c
Kirill <freshjs.ftd@gmail.com>
2021-10-20 17:05:36 +0300

    Add redux hooks

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,37 +1,34 @@
-import { connect } from 'react-redux';
+import { connect, useSelector, useDispatch } from 'react-redux';
 import * as actionCreators from '../../actions/counterCreators';
+import { bindActionCreators } from 'redux';
 
 function Counter(props) {
-  const { count, step, incrementAction, decrementAction, setStepAction } =
-    props;
+  const { step, count } = useSelector((state) => {
+    const {
+      counter: { step, count },
+    } = state;
+
+    return { step, count };
+  });
+  const dispatch = useDispatch();
+
+  const { increment, decrement, setStep } = bindActionCreators(
+    actionCreators,
+    dispatch
+  );
 
   const changeStep = ({ target: { value } }) => {
-    setStepAction(Number(value));
+    setStep(Number(value));
   };
 
   return (
     <div>
       <h1>Counter is: {count}</h1>
       <input value={step} onChange={changeStep} />
-      <button onClick={decrementAction}>Отнять</button>
-      <button onClick={incrementAction}>Добавить</button>
+      <button onClick={decrement}>Отнять</button>
+      <button onClick={increment}>Добавить</button>
     </div>
   );
 }
 
-function mapStateToProps(state) {
-  const {
-    counter: { step, count },
-  } = state;
-  return { step, count };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    incrementAction: () => dispatch(actionCreators.increment()),
-    decrementAction: () => dispatch(actionCreators.decrement()),
-    setStepAction: (newStep) => dispatch(actionCreators.setStep(newStep)),
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Counter);
+export default Counter;
